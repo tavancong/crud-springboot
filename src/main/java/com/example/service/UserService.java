0180c0388efee3b535f44693a3f3766cad8b4adb
Vancong01 <tavancong1994@gmail.com>
2023-03-13 15:46:03 +0700

    crud

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import com.example.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    List<User> getAllUser();
+    void saveUser(User user);
+    void deleteUser(Long id);
+    Optional<User> findUserById(Long id);
+}
