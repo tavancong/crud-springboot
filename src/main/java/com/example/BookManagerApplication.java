8eaaac6f38b0341b7aaf664ac112c62c2b913ea9
Vancong01 <tavancong1994@gmail.com>
2023-03-22 11:05:08 +0700

    security

--- src/main/java/com/example/BookManagerApplication.java
+++ src/main/java/com/example/BookManagerApplication.java
@@ -3,12 +3,12 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 
 @EnableJpaRepositories("com.example.repository")
 @EntityScan("com.example.entity")
-@SpringBootApplication
+@SpringBootApplication()
 public class BookManagerApplication {
 
     public static void main(String[] args) {
